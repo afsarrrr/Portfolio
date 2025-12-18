59a0bc49be6c2d20f7f1665ac84fc0ebbc8328f0
Afsarrrr <hornymike4@gmail.com>
2025-12-18 20:02:46 +0530

    snow flakes are added in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,30 @@ import About from "./components/About";
 import Skill from "./components/Skill";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-
+import Snowfall from 'react-snowfall'
+import { redirect } from "react-router-dom";
 function App() {
   return (
     <>
   <Navbar />
+ <Snowfall
+        color="#E6FBFF"
+        snowflakeCount={400}
+        speed={[0.6, 1.4]}
+        wind={[-0.3, 0.3]}
+        radius={[1, 3]}
+        opacity={[0.6, 1]}
+        rotationSpeed={[-1, 1]}
+        changeFrequency={200}
+        style={{
+          position: "fixed",
+          width: "100vw",
+          height: "100vh",
+          zIndex: 10,
+          pointerEvents: "none",
+        }}
+      />
+
 
   <main className="pt-[72px]">
     <section id="home" className="scroll-mt-[72px]">
